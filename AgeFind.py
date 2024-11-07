d57407e235d3700363b926a02d73c35e6d8250c0
Phalgun Soni <phalgunsoni109@gmail.com>
2024-11-07 17:55:47 +0530

    Add files via upload

---
+++ AgeFind.py
@@ -0,0 +1,15 @@
+Birth=int(input("Enter your Birth Year "))
+
+Current_Year=int(input("Current Year "))
+
+p=Current_Year-Birth
+print("Your Age ",p) 
+
+
+
+
+
+"""from datetime import date
+Year=int(input("Enter your Year of birth "))
+Current_Date = date.today().strftime("%Y")
+print("Age",int(Current_Date)-Year)"""
